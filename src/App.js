e5b85b24e69d8e13bdf366402d5af5fadd33f739
Marina Metanova <101000570+marina-metanova@users.noreply.github.com>
2022-07-24 13:34:01 +0300

    Add reacr-router-dom to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
@@ -8,7 +10,10 @@ function App() {
             <Header></Header>
 
             <main id="main-content">
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                
             </main>
         </div>
     );
